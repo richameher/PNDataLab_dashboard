a6c6f110a1477fff725427f496f4ffb63bf67997
Richa Meherwal <meherwa2@illinois.edu>
2022-11-08 14:48:49 -0800

    Updated APIs and requests to API

--- app.py
+++ app.py
@@ -6,24 +6,104 @@ app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
+# Datasets and acceptable Tasks
+datasets = {0:"Iris Dataset", 1:"Diabetes Dataset", 2:"Sample"}
 
-@app.route("/api/dashboard", methods=["GET"])
+metadata = {0: ["SepalWidth", "SepalLengthCm"], 1:["Age", "Height"]}
+# tasks table task_id, task_name
+tasks = {0:"Regression", 1:"Classification", 2:"Unsupervised"}
+
+# relationshio table for each dataset and task
+dataset_tasks = {0:[0, 1, 2], 1:[0]}
+
+# tasks and algorithms
+# algorithms table alg_id, alg_name
+algorithms = {0:"Linear Regression", 1:"PCA", 2:"KNN", 3:"Naive Bayes"}
+
+# algorithm and task relationship task_id algorith id
+tasks_algos = {0:[0], 1:[2,3], 2:[1]}
+
+
+@app.route("/api/get_datasets", methods=["GET"])
 @cross_origin()
-def fecth_algorithm_type():
-    dict_result = {}
-    dict_result["tasks"]=["Regression","Classification"]
-    dict_result["datasets"]=["Dataset1","Dataset2"]
-    dict_result["algorithms"]=["Alg1","Alg2"]
-    print("fetched_types",json.dumps({"types":["Regression","Classification"]}))
-    return json.dumps(dict_result),200
+def get_datasets():
+    # do operation for datasets
+    # if we connect to mysql server this would fectch the whole datasets table
 
+    response = datasets
+    try:
 
-@app.route("/api/get_metadata", methods=["GET"])
+        return json.dumps(response),200
+    except:
+        return json.dumps({"error": "No datasets found"}), 400
+
+@app.route("/api/get_columns_tasks/<dataset_id>", methods=["GET"])
 @cross_origin()
-def get_metadata():
-    dict_result = {}
-    dict_result["columns"]=["SepalLengthCm","SepalWidth","PetalLengthCm","PetalWidthCm"]
-    return json.dumps(dict_result),200
+def get_columns_tasks(dataset_id):
+    # do operation for datasets
+    # if we connect to mysql server this would fectch the whole datasets table
+    # try:
+    print("Retrive data for", type(dataset_id), dataset_tasks[0])
+    dataset_id = int(dataset_id)
+    tasks_ids = dataset_tasks[dataset_id]
+    task_dict = {}
+    response = {}
+
+    for task_id in tasks_ids:
+        # form a new dict that contains the tasks operable on dataset
+        task_dict[task_id]= tasks[task_id]
+
+    response["tasks"]= task_dict
+
+    columns = metadata[dataset_id]
+
+    response["columns"] = columns
+
+    print(response)
+
+    return json.dumps(response),200
+
+@app.route("/api/get_algorithms/<task_id>", methods=["GET"])
+@cross_origin()
+def get_algorithms(task_id):
+    # do operation for datasets
+    # if we connect to mysql server this would fectch the whole datasets table
+
+    task_id = int(task_id)
+    # task and algos
+    algorithms_ids = tasks_algos[task_id]
+
+    algorithms_dict = {}
+
+    response = {}
+
+    for algo_id in algorithms_ids:
+        # form a new dict that contains the tasks operable on dataset
+        algorithms_dict[algo_id]= algorithms[algo_id]
+
+    response["algorithms"]= algorithms_dict
+
+    return json.dumps(response),200
+    # except:
+    #     return json.dumps({"error": "Error occured when retriving tasks for this dataset"}), 400
+
+# @app.route("/api/dashboard", methods=["GET"])
+# @cross_origin()
+# def fecth_algorithm_type():
+#     dict_result = {}
+#     dict_result["tasks"]=["Regression","Classification"]
+#     dict_result["datasets"]=["Dataset1","Dataset2"]
+#     dict_result["algorithms"]=["Alg1","Alg2"]
+#     print("fetched_types",json.dumps({"types":["Regression","Classification"]}))
+#     return json.dumps(dict_result),200
+#
+#
+# @app.route("/api/get_metadata", methods=["GET"])
+# @cross_origin()
+# def get_metadata():
+#     dict_result = {}
+#     dict_result["columns"]=["SepalLengthCm","SepalWidth","PetalLengthCm","PetalWidthCm"]
+#     return json.dumps(dict_result),200
 
 # main driver function
 if __name__ == '__main__':
--- src/components/Drevised.js
+++ src/components/Drevised.js
@@ -4,6 +4,8 @@ import { FormGroup, Checkbox, FormControlLabel, Button} from '@mui/material';
 import PlayCircleIcon from '@mui/icons-material/PlayCircle';
 import Paper from '@mui/material/Paper';
 import { styled } from '@mui/material/styles';
+
+
 const Item = styled(Paper)(({ theme }) => ({
   backgroundcolor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
   ...theme.typography.body2,
@@ -18,28 +20,67 @@ export default class Drevised extends React.Component {
       super(props);
       // load state from previous configuration
       this.state = {
-        dashboard: null,
-        tasks: ["None"],
-        datasets:["None"],
-        algorithms:["None"],
-        algorithm:[""],
-        dataset: [""],
-        task:[""],
-        disabled_one:false,
-        loading: false,
-        disabled_two: true,
-        disabled_three: true,
-        columns: [],
-        submitEnable: false
+        tasks: ["None"], // store tasks loaded from API
+        datasets:[], // store datasets loaded from API
+        algorithms:["None"], // store algorithms loaded from API
+        algorithm_value:"", // stores algorithm value chosen
+        dataset_value: "", // stores dataset value chosen
+        task_value:"", // stores task_value chosen
+        disabled_dataset:false, // first allow only dataset selection
+        disabled_task: true, // then allow task selection
+        disabled_algo: true, // then allow algo selection
+        loading: false, // loads when api call is made to run the algorithm on dataset
+        columns: [], // stores columns from dataset
+        column_x_checked: null, //store the checked x and y column for some algorithms
+        column_y_checked: null,
+        columns_checked: [], // store the checked columns for PCA like algorithms
+        disable_submit_button: false
       }
   }
 
   componentDidMount() {
-        this.getConfigOptions();
+        this.getDatasets();
     }
 
-    getConfigOptions = () => {
+    getDatasets = () => {
+      var xhr = new XMLHttpRequest();
+      xhr.onreadystatechange = function () {
+          // Only run if the request is complete
+          if (xhr.readyState !== 4) {
+              return;
+          }
+          if (xhr.status >= 200 && xhr.status < 300) {
+              let response = JSON.parse(xhr.responseText);
+              // datasets are key value dictionary
+              this.setState({datasets: response});
+          } else {
+              let response = JSON.parse(xhr.responseText)
+              // notify if error
+              this.setState({errorFromServer: response.error});
+          }
+        }.bind(this);
+        xhr.open("GET", "http://127.0.0.1:5000/api/get_datasets");
+        xhr.send();
+
+    }
+
+    handleChangeDatasets =(event)=>{
+      this.setState({dataset_value: event.target.value,
+                    tasks:[""], // reset task
+                    disabled_task: false, // enable task
+                    disabled_algo: true,
+                    disable_submit_button: true,
+                    task_value:"",
+                    algorithm_value:"" }); // disable algo and submit button
+      // console.log("Setting dataset value as:", event.target.value);
+      // this.getDataSetcolumns(); // get dataset columns
+      this.getColsandTasks(event.target.value);
+
+    }
+
+    getColsandTasks = (dataset_id) => {
           var xhr = new XMLHttpRequest();
+
           xhr.onreadystatechange = function () {
               // Only run if the request is complete
               if (xhr.readyState !== 4) {
@@ -48,20 +89,22 @@ export default class Drevised extends React.Component {
 
               if (xhr.status >= 200 && xhr.status < 300) {
                   let response = JSON.parse(xhr.responseText);
+
+                  this.setState({columns: response.columns, tasks: response.tasks});
                   console.log(response);
-                  this.setState({datasets: response.datasets, algorithms:response.algorithms, tasks: response.tasks});
               } else {
                   let response = JSON.parse(xhr.responseText)
                   this.setState({errorFromServer: response.error});
               }
           }.bind(this);
 
-          xhr.open("GET", "http://127.0.0.1:5000/api/dashboard");
+          xhr.open("GET", "http://127.0.0.1:5000/api/get_columns_tasks/"+dataset_id);
           xhr.send();
       }
 
-      getDataSetcolumns = () => {
+      getAlgorithms = (task_id) => {
             var xhr = new XMLHttpRequest();
+
             xhr.onreadystatechange = function () {
                 // Only run if the request is complete
                 if (xhr.readyState !== 4) {
@@ -70,36 +113,33 @@ export default class Drevised extends React.Component {
 
                 if (xhr.status >= 200 && xhr.status < 300) {
                     let response = JSON.parse(xhr.responseText);
+
+                    this.setState({algorithms: response.algorithms});
                     console.log(response);
-                    this.setState({columns: response.columns});
                 } else {
                     let response = JSON.parse(xhr.responseText)
                     this.setState({errorFromServer: response.error});
                 }
             }.bind(this);
 
-            xhr.open("GET", "http://127.0.0.1:5000/api/get_metadata");
+            xhr.open("GET", "http://127.0.0.1:5000/api/get_algorithms/"+task_id);
             xhr.send();
         }
 
       handleChangeTask = (event) => {
-        this.setState({task: event.target.value,
-                      dataset:[""],
-                      algorithm:[""],
-                      disabled_two: false,
-                      disabled_three: true, columns:[]})
-      }
-
-      handleChangeDatasets =(event)=>{
-        this.setState({dataset: event.target.value,
-                      algorithm:[""],
-                      disabled_three: false});
-        this.getDataSetcolumns();
+        this.setState({task_value: event.target.value,
+                      algorithms:[""], // reset task
+                      disabled_algo: false, // enable task
+                      disable_submit_button: false,
+                      algorithm_value:"" });
+        this.getAlgorithms(event.target.value);
 
       }
+
+
 
       handleChangeAlgo = (event) =>{
-        this.setState({algorithm: event.target.value})
+        this.setState({algorithm_value: event.target.value})
 
       }
 
@@ -107,53 +147,54 @@ export default class Drevised extends React.Component {
     return (
       <div>
       <Item>
-      <FormControl style={{
-        margin: '10px'
-      }} fullWidth disabled={this.state.disabled_one}>
-      <InputLabel id="selectlabel">Select Tasks</InputLabel>
-      <Select
-        labelid="selectlabel"
-        value={this.state.task}
-        label="Tasks"
-        onChange={this.handleChangeTask}
-      >
-     <MenuItem value=""></MenuItem>
-      {this.state.tasks.map((algorithm,idx) => (
-        <MenuItem value={algorithm} key={idx}>{algorithm}</MenuItem>
-      ))}
-      </Select>
-      </FormControl>
 
       <FormControl style={{
         margin: '10px'
-      }} fullWidth disabled={this.state.disabled_two}>
-      <InputLabel id="selectlabeld">Select Dataset</InputLabel>
+      }} fullWidth disabled={this.state.disabled_dataset}>
+      <InputLabel id="select-label-dataset">Select Dataset</InputLabel>
       <Select
-        labelid="selectlabeld"
-        value={this.state.dataset}
-        label="Dataset"
+        labelid="select-label-dataset"
+        value={this.state.dataset_value}
+        label="select-dataset"
         onChange={this.handleChangeDatasets}
       >
-     <MenuItem value=""></MenuItem>
-      {this.state.datasets.map((dataset,idx) => (
-        <MenuItem value={dataset} key={idx}>{dataset}</MenuItem>
+
+      {Object.keys(this.state.datasets).map((keyName, idx) => (
+        <MenuItem value={keyName} key={keyName}>{keyName} | {this.state.datasets[keyName]}</MenuItem>
       ))}
       </Select>
       </FormControl>
 
       <FormControl style={{
         margin: '10px'
-      }} fullWidth disabled={this.state.disabled_three}>
-      <InputLabel id="selectlabelA">Select Algorithm</InputLabel>
+      }} fullWidth disabled={this.state.disabled_tasks}>
+      <InputLabel id="select-label-task">Select Task</InputLabel>
       <Select
-        labelid="selectlabelA"
-        value={this.state.algorithm}
-        label="Algorithm"
+        labelid="select-label-task"
+        value={this.state.task_value}
+        label="select-task"
+        onChange={this.handleChangeTask}
+      >
+
+      {Object.keys(this.state.tasks).map((keyName, idx) => (
+        <MenuItem value={keyName} key={keyName}>{keyName} | {this.state.tasks[keyName]}</MenuItem>
+      ))}
+      </Select>
+      </FormControl>
+
+      <FormControl style={{
+        margin: '10px'
+      }} fullWidth disabled={this.state.disabled_algo}>
+      <InputLabel id="select-label-algo">Select Algorithm</InputLabel>
+      <Select
+        labelid="select-label-algo"
+        value={this.state.algorithm_value}
+        label="select-algo"
         onChange={this.handleChangeAlgo}
       >
-     <MenuItem value=""></MenuItem>
-      {this.state.algorithms.map((dataset,idx) => (
-        <MenuItem value={dataset} key={idx}>{dataset}</MenuItem>
+
+      {Object.keys(this.state.algorithms).map((keyName, idx) => (
+        <MenuItem value={keyName} key={keyName}>{keyName} | {this.state.algorithms[keyName]}</MenuItem>
       ))}
       </Select>
       </FormControl>
@@ -164,8 +205,7 @@ export default class Drevised extends React.Component {
       <InputLabel id="select-columns">Select columns</InputLabel>
       <FormGroup  labelid="select-columns" style={{
         margin: 'auto',
-        height:'300px',
-        'overflow-y': 'scroll'
+        height:'300px'
       }} >
       {this.state.columns.map((column, idx) => (
         <FormControlLabel key={idx} control={<Checkbox />} label={column} />
